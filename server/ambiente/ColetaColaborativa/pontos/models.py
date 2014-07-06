bcd6d20425d4cb75192e580ea44ce9851e818175
Thiago Silva <thiagodd.silva@gmail.com>
2014-07-05 22:59:28 -0300

    Alteração do moledo, retirada do model Local

--- server/ambiente/ColetaColaborativa/pontos/models.py
+++ server/ambiente/ColetaColaborativa/pontos/models.py
@@ -33,9 +33,11 @@ class Ponto(models.Model):
     longitude = models.DecimalField('Longitude', max_digits=10, decimal_places=8)
     ponto_privado = models.BooleanField('Ponto Privado', default=False)
     descricao = models.TextField('Descrição')
+    observacao = models.TextField('Observações')
 
     #Associação entre ponto e usuário que cadastrou o ponto.
     usuario = models.ForeignKey(Usuario)
+    tipos = models.ManyToManyField(Tipo)
 
     class Meta:
         verbose_name = u'Ponto'
@@ -45,54 +47,16 @@ class Ponto(models.Model):
         return self.descricao[0:50] + '...'
 
     def get_dicionario(self):
-        lista_dicionarios_locais = []
-
-        try:
-            locais = Local.objects.filter(ponto__id=self.id)
-
-            if len(locais) > 0:
-                for local in locais.values():
-                    lista_dicionarios_locais.append(local)
-
-        except Local.DoesNotExist:
-            pass
-
         retorno = {
             'latitude' : str(self.latitude),
             'longitude' : str(self.longitude),
             'ponto_privado' : self.ponto_privado,
             'descricao' : self.descricao,
-            'usuario' : self.usuario.get_dicionario()
+            'usuario' : self.usuario.get_dicionario(),
         }
 
-        if len(lista_dicionarios_locais) != 0:
-            retorno['locais'] = lista_dicionarios_locais
-
-        return retorno
-
-'''
-Model que mapeia um Local de Coleta.
-'''
-class Local(models.Model):
-    observacao = models.TextField('Observações')
-
-    #Chave para o model Tipo.
-    tipo = models.ForeignKey(Tipo)
-
-    #Chave para o model Ponto.
-    ponto = models.ForeignKey(Ponto)
-
-    class Meta:
-        verbose_name = u'Local de Coleta'
-        verbose_name_plural = u'Locais de Coleta'
-
-    def __unicode__(self):
-        return self.tipo
-
-    def get_dicionario(self):
-        retorno ={
-            'tipo' : self.tipo.nome,
-            'observacao' : self.observacao
-        }
-
+        retorno['tipos'] = []
+        for tipo in self.tipos.iterator():
+            retorno['tipos'].append(tipo.get_dicionario())
+        
         return retorno
